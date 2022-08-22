20b5827fb9bb8cc118899c6b505ccd1ea95f50e0
Elkhan911 <955pro@mail.ru>
2022-08-22 14:59:23 +0500

    js - random func + js - user choose

---
+++ script.js
@@ -0,0 +1,37 @@
+const columnsAll = document.querySelectorAll(".table__column");
+// Выбор комьютера
+const compChoose1 = getRandom(1, 20);
+console.log(compChoose1);
+const compChoose2 = getRandom(1, 20);
+console.log(compChoose2);
+const compChoose3 = getRandom(1, 20);
+console.log(compChoose3);
+
+// Счетчик для id ячеек
+let i = 0;
+
+for (let column of columnsAll) {
+  i++;
+  column.id = i;
+  column.addEventListener("click", userChoose);
+  console.log(column.id);
+}
+
+// Функция для выбора пользователя
+function userChoose() {
+  this.classList.add("clickOn");
+  this.removeEventListener("click", userChoose);
+}
+
+function userColumnId() {
+  this.addEventListener("click", function () {
+    return this.id;
+  });
+}
+
+// функция Random
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
