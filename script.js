d6eda30c3440de20c7d2a96458000a1f8f4b0877
Elkhan911 <955pro@mail.ru>
2022-08-22 23:22:29 +0500

    comp choices

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 const columnsAll = document.querySelectorAll(".table__column");
-// Выбор комьютера
-const compChoose1 = getRandom(1, 20);
-console.log(compChoose1);
-const compChoose2 = getRandom(1, 20);
-console.log(compChoose2);
-const compChoose3 = getRandom(1, 20);
-console.log(compChoose3);
+// Массив выборово юзера
+const userChoices = [];
+// Массив выборов комьютера
+const compChoices = [];
+console.log(compChoices);
 
 // Счетчик для id ячеек
-let i = 0;
+let counter = 0;
 
 // присваиваем каждой ячейке id
 for (let column of columnsAll) {
-  i++;
-  column.id = i;
+  counter++;
+  column.id = counter;
   column.addEventListener("click", userChoose);
-}
-
-// Функция для выбора пользователя
-function userChoose() {
-  this.classList.add("clickOn");
-  this.removeEventListener("click", userChoose);
-  return this.id;
 }
 
 // функция Random
@@ -31,5 +22,31 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-// пытаемся проверить совпадает ли выбор юзера и компьютера ???????????
-console.log(userChoose() == compChoose1);
+// Функция для массива выборов пользователя
+function userChoose() {
+  userChoices.push(this.id);
+  console.log(compChoices.includes(Number(this.id)));
+
+  if (compChoices.includes(Number(this.id))) {
+    this.classList.add("succes");
+  } else {
+    this.classList.add("fail");
+  }
+
+  console.log(compChoices);
+  console.log(userChoices);
+  this.removeEventListener("click", userChoose);
+}
+
+function compChoose() {
+  for (let i = 1; i < 11; i++) {
+    if (!compChoices.includes(getRandom(1, 100))) {
+      compChoices.push(getRandom(1, 100));
+    } else {
+      compChoices.push(getRandom(1, 100));
+    }
+  }
+  console.log(compChoices);
+}
+
+compChoose();
